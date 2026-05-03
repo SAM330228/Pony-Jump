89a364ce32ce1f06397d135c68da528050382b88
SAM330228 <Artemii2012Elizarov2012@yandex.ru>
2026-05-03 14:07:30 +0300

    Large update.py
    added 2 classes, added Flutter Shy.

--- Pony-Jump.py
+++ Pony-Jump.py
@@ -5,7 +5,40 @@ screen = display.set_mode((x,y))
 display.set_caption('Pony Jump')
 background = transform.scale(image.load('clouds.png'), (x,y))
 
+mixer.init()
+#!mixer.music.load('.mp3')
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_s] and self.rect.y < 635:
+            self.rect.y += self.speed
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 435:
+            self.rect.x += self.speed
+
+FlutterShy = Player('Pony.jpg', 225, 500, 5)
+
+class Wall(GameSprite):
+    def update_w():
+        None
+    def spawn():
+        None
+#игра
 game = True
 clock = time.Clock()
 FPS = 60
@@ -16,6 +49,9 @@ while game:
             game = False
     screen.blit(background, (0,0))
     if not finished:
-        None
+        
+        FlutterShy.update()
+        FlutterShy.reset()
+
     clock.tick(FPS)
     display.update()
